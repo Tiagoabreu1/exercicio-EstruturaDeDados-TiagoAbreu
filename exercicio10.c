31802cf9ef7b6b785fafa8b900eebcb3921fb15a
Tiago Abreu <abreu.titiago@gmail.com>
2024-03-19 11:13:38 -0300

    Arruma o problema de numeros negativos

--- exercicio10.c
+++ exercicio10.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 int calcularQuociente(int n1, int n2) {
+    int controle = 1;
+    if (n1 < 0) {
+        n1 = n1 * -1;
+        controle = controle * -1;
+    }
+    if (n2 < 0) {
+        n2 = n2 * -1;
+        controle = controle * -1;
+    }
     if (n1 < n2) {
         return 0;
     } else {
-        return 1 + calcularQuociente(n1 - n2, n2);
+        return 1 * controle + calcularQuociente(n1 - n2, n2);
     }
 }
 
